8df39bda3f766c54c0e597b99c7b2b73e9822470
Hp990 <65535038+Hp990@users.noreply.github.com>
2021-09-06 17:58:03 +0530

    Create chat_model.dart

---
+++ chat_model.dart
@@ -0,0 +1,16 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+  List<ChatModel> dummyData=[
+    new ChatModel(
+      name: "Himanshu Pradhan",
+      message: "Hello didi",
+      time: "18:00",
+      avatarUrl: "D:\Family\HIMANSHU",
+    )
+  ];
